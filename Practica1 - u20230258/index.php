ad8d140000e2ff51c6d4587e0d050204eb254451
Jorge Lara <140337814+JorgeLaraR@users.noreply.github.com>
2024-08-15 23:12:13 -0600

    Practica 1 - u20230258

---
+++ Practica1 - u20230258/index.php
@@ -0,0 +1,44 @@
+<?php
+
+echo "holis";
+
+//Cadena
+$nombre = "Jorge";
+$apellido = "Lara";
+
+
+//Entero
+$edad = "24";
+
+
+//Float
+$estatura = 1.70;
+
+
+//booleano
+$vivo = true;
+
+
+//imprimir en php
+echo $nombre . "<br>";
+print_r($apellido);
+
+echo "<br><br>";
+
+echo $nombre, ' ', $apellido, ' ', $estatura;
+
+echo "<br><br>";
+
+var_dump($edad);
+
+$edad = 'veinticuatro';
+
+var_dump($edad);
+
+echo "<br><br>";
+
+echo 'Mi nombre es ' . $nombre . ' ' . $apellido;
+
+echo "<br><br>";
+
+echo "<input type='\"text"'>;
